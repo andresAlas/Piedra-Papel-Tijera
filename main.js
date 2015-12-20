f4dca9417857f274de5114dbb0d7759327338787
andres <andres.alas@hotmail.com>
2015-12-19 20:48:35 -0600

    Agrege las variables y condicionales

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function aleatoria(maximo, minimo)
+function aleatorio(maximo, minimo)
 {
 	var numero = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 	return numero;
@@ -7,3 +7,63 @@ function aleatoria(maximo, minimo)
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
+
+var opciones = [piedra, papel, tijera];
+
+var opcionesUsuario;
+var opcionMaquina = aleatorio(0,2);
+
+opcionesUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+alert("Elegiste" + opciones[opcionesUsuario]);
+alert("Javacript eligio" opciones[opcionMaquina]);
+
+if(opcionUsuario == piedra)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Perdiste :( ");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Ganaste!");
+    }
+}
+else if(opcionUsuario == papel)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Perdiste!");
+    }
+}
+else if(opcionUsuario == tijera)
+{
+    if(opcionMaquina == piedra)
+    {
+        alert("Perdiste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Empate!");
+    }
+}
+else
+{
+    alert("¿Que Putas");
+}
