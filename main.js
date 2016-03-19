58901403750d5162924a85ad9cbd035d50724b6d
andres <andres.alas@hotmail.com>
2016-03-19 14:19:27 -0600

    Algunos cambios

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var papel = 1;
 var tijera = 2;
 var lagarto = 3;
 var spock = 4;
-var opciones = ["piedra", "papel", "tijera", "lagarto", "spock"];
+var opciones = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
 var opcionUsuario;
 var opcionMaquina = aleatorio(0,4);
 
@@ -18,38 +18,41 @@ function inicio()
 
 }
 
-opcionUsuario = prompt("¿Qué eliges?\n Piedra: 0\n Papel: 1\n Tijera: 2\n Lagarto: 3\n  Spock: 4", 0);
-alert("Elegiste: " + opciones[opcionUsuario]);
+opcionUsuario = prompt("Que eliges: \n Piedra: 0 \n Papel: 1 \n Tijera: 2 \n Lagarto: 3 \n Spock: 4", 0);
+alert("Eligiste: " + opciones[opcionUsuario]);
 alert("Javacript eligio: " + opciones[opcionMaquina]);
 
-if(opcionMaquina == opcionUsuario){ alert("Empate")};
-
-if(opcionUsuario == piedra && opcionMaquina == tijera){alert("Ganaste")};
-if(opcionUsuario == piedra && opcionMaquina == lagarto){alert("Ganaste")};
-
-if(opcionUsuario == papel && opcionMaquina == piedra){alert("Ganaste")};
-if(opcionUsuario == papel && opcionMaquina == spock){alert("Ganaste")};
-
-if(opcionUsuario == tijera && opcionMaquina == papel){alert("Ganaste")};
-if(opcionUsuario == tijera && opcionMaquina == spock){alert("Ganaste")};
-
-if(opcionUsuario == lagarto && opcionMaquina == papel){alert("Ganaste")};
-if(opcionUsuario == lagarto && opcionMaquina == spock){alert("Ganaste")};
-
-if(opcionUsuario == spock && opcionMaquina == piedra){alert("Ganaste")};
-if(opcionUsuario == spock && opcionMaquina == tijera){alert("Ganaste")};
-
-if(opcionUsuario == piedra && opcionMaquina == papel){alert("Perdiste")};
-if(opcionUsuario == piedra && opcionMaquina == spock){alert("Perdiste")};
-
-if(opcionUsuario == papel && opcionMaquina == tijera){alert("Perdiste")};
-if(opcionUsuario == papel && opcionMaquina == lagarto){alert("Perdiste")};
-
-if(opcionUsuario == tijera && opcionMaquina == piedra){alert("Perdiste")};
-if(opcionUsuario == tijera && opcionMaquina == spock){alert("Perdiste")};
-
-if(opcionUsuario == lagarto && opcionMaquina == tijera){alert("Perdiste")};
-if(opcionUsuario == lagarto && opcionMaquina == piedra){alert("Perdiste")};
-
-if(opcionUsuario == spock && opcionMaquina == lagarto){alert("Perdiste")};
-if(opcionUsuario == spock && opcionMaquina == papel){alert("Perdiste")};
+if(opcionMaquina == opcionUsuario)
+{ 
+	alert("Empate")
+}
+else if(opcionUsuario == piedra && opcionMaquina == tijera ||
+   opcionUsuario == piedra && opcionMaquina == lagarto ||
+   opcionUsuario == papel && opcionMaquina == piedra ||
+   opcionUsuario == papel && opcionMaquina == spock ||
+   opcionUsuario == tijera && opcionMaquina == papel ||
+   opcionUsuario == tijera && opcionMaquina == lagarto ||
+   opcionUsuario == lagarto && opcionMaquina == papel ||
+   opcionUsuario == lagarto && opcionMaquina == spock ||
+   opcionUsuario == spock && opcionMaquina == piedra ||
+   opcionUsuario == spock && opcionMaquina == tijera)
+{
+	alert("Ganaste :)");
+}
+else if(opcionUsuario == piedra && opcionMaquina == papel ||
+   opcionUsuario == piedra && opcionMaquina == spock ||
+   opcionUsuario == papel && opcionMaquina == tijera ||
+   opcionUsuario == papel && opcionMaquina == lagarto ||
+   opcionUsuario == tijera && opcionMaquina == piedra ||
+   opcionUsuario == tijera && opcionMaquina == spock ||
+   opcionUsuario == lagarto && opcionMaquina == tijera ||
+   opcionUsuario == lagarto && opcionMaquina == piedra ||
+   opcionUsuario == spock && opcionMaquina == lagarto ||
+   opcionUsuario == spock && opcionMaquina == papel)
+{
+	alert("Perdiste .l.");
+}
+else
+{
+	alert("Que estas haciendo pedazo de idiota");
+}
